f714e858afee63ee0323c9ee43e90ea40d133b01
James Wang <xuwan>
2019-04-10 22:04:08 +1000

    [ADD] axios into project

---
+++ src/views/Temp.vue
@@ -0,0 +1,36 @@
+<style lang="less" scoped="scoped">
+@import '../assets/css/common.less';
+</style>
+<template>
+	<div class="page-index">
+		
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'home',
+    components: {},
+    data () {
+    	return {}
+    },
+    methods: {
+    	getData(){
+    		this.$ajax({
+				method: 'GET', 
+				url: 'melbourneGeoInfo.json', 
+				data: {},
+				showLoading: true
+			}).then(res => {
+        console.log(res)
+			}, error => {
+        console.log('error')
+	        })
+    	}
+    },
+    created () {},
+	mounted(){
+		
+	}
+}
+</script>
