16d3aa34dfeda441fc483efa8d76c09bc42eae44
zhanglihuan <lihuanz@student.unimelb.edu.au>
2019-05-02 22:25:02 +1000

    add couch db

--- backend/backend/api/urls.py
+++ backend/backend/api/urls.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from django.urls import path, re_path
-from backend.api.views.tweets import tweet_pic_router, tweet_router
+from backend.api.views.tweets import tweet_router
+from backend.api.views.tweet_pics import tweet_pic_router
+
 
 urlpatterns = [
     path('tweet/pic/<str:resource>/', tweet_pic_router),
