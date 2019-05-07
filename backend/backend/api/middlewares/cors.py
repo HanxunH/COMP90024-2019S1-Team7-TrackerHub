07f179f45c37d3692d97fe4e5bab0722b5090ca3
zhanglihuan <lihuanz@student.unimelb.edu.au>
2019-05-07 23:14:03 +1000

    [FIX] ACAO8

--- backend/backend/api/middlewares/cors.py
+++ backend/backend/api/middlewares/cors.py
@@ -7,7 +7,7 @@ class CrosMeddleware(MiddlewareMixin):
 
     def process_response(self, request, response):
         response['Access-Control-Allow-Origin'] = '*'
-        response['Access-Control-Allow-Methods'] = 'GET, OPTIONS, POST'
+        response['Access-Control-Allow-Methods'] = 'GET,OPTIONS,POST'
         response['Access-Control-Allow-Headers'] = '*'
         response['Access-Control-Max-Age'] = 3628800
         return response
